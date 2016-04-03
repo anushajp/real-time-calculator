d8002912438cc53b2759f32a5af7410712c64fb0
Afzal Shahul Hameed <aflibnush@gmail.com>
2016-04-03 23:12:44 +0530

    Adds a basic template

--- calculator/views.py
+++ calculator/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 class Home(View):
     def get(self, request):
-        return HttpResponse("Welcome!")
+        return render(request, 'calculator/home.html', locals())
