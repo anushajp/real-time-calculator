de88e9e97c2ea94f4ad355aab981f08790dedca6
Afzal Shahul Hameed <aflibnush@gmail.com>
2016-04-04 14:29:34 +0530

    Implements updation and storing of calculations

--- calculator/static/calculator/js/home.js
+++ calculator/static/calculator/js/home.js
@@ -1,3 +1,23 @@
 function calculateInput (toCalculate) {
-    Calc.Input.value = eval(toCalculate);
+    var csrftoken = getCookie('csrftoken');
+    var answer = eval(toCalculate);
+
+    Calc.Input.value = answer;
+
+    $.ajax({
+        type: "POST",
+        data: {
+            csrfmiddlewaretoken: csrftoken,
+	    to_calculate : toCalculate,
+	    answer: answer,
+        },
+
+        success: function(json) {
+	    console.log(json['message'])
+	},
+
+        error: function(xhr, err) {
+            console.log(xhr.responseText);
+        }
+    });
 }
--- calculator/views.py
+++ calculator/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from calculator.models import Calculation
 
 class Home(View):
     def get(self, request):
         return render(request, 'calculator/home.html', locals())
+
+    def post(self, request):
+        if request.is_ajax:
+            try:
+                calculation = Calculation.objects.create(
+                    to_calculate=request.POST.get('to_calculate'),
+                    answer=request.POST.get('answer')
+                )
+                return JsonResponse({"message": "success"})
+            except (Exception) as e:
+                return JsonResponse({"message": "failed with error; '{0}'".format(e)})
