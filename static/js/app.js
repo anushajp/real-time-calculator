732dcd72895b4230682dce3b6a70c33e9cb81fcf
Afzal Shahul Hameed <aflibnush@gmail.com>
2016-04-04 13:31:01 +0530

    Desired listing of calcualtions done via site

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,9 @@ var TodoApp = angular.module('RTCalcApp', [
     'SwampDragonServices',
     'RTCalcControllers'
 ]);
+
+TodoApp.filter('reverse', function() {
+    return function(items) {
+	return items.slice().reverse();
+    };
+});
